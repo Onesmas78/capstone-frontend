2b62bcb1a9c4936a398b3bc1e340dad82babf8a8
onesmas <onesmas78@gmail.com>
2023-07-28 10:43:33 +0300

    updated palette

--- src/scenes/navbar/index.jsx
+++ src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          DIT Base
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
--- src/scenes/widgets/AdvertWidget.jsx
+++ src/scenes/widgets/AdvertWidget.jsx
@@ -12,24 +12,23 @@ const AdvertWidget = () => {
     <WidgetWrapper>
       <FlexBetween>
         <Typography color={dark} variant="h5" fontWeight="500">
-          Sponsored
+          Upcoming Events
         </Typography>
-        <Typography color={medium}>Create Ad</Typography>
+        <Typography color={medium}>Create New Event</Typography>
       </FlexBetween>
       <img
         width="100%"
         height="auto"
         alt="advert"
-        src="http://localhost:3001/assets/info4.jpeg"
+        src="http://localhost:4001/assets/image002.jpg"
         style={{ borderRadius: "0.75rem", margin: "0.75rem 0" }}
       />
       <FlexBetween>
-        <Typography color={main}>MikaCosmetics</Typography>
-        <Typography color={medium}>mikacosmetics.com</Typography>
+        <Typography color={main}>Tribe Review</Typography>
+        <Typography color={medium}>DIT Tribe Review</Typography>
       </FlexBetween>
       <Typography color={medium} m="0.5rem 0">
-        Your pathway to stunning and immaculate beauty and made sure your skin
-        is exfoliating skin and shining like light.
+        Thursday, 03/08/2023
       </Typography>
     </WidgetWrapper>
   );
